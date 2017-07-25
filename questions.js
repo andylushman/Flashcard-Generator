3f34f08df929fdccdccfa031dc7da7c8475a38b8
Andy Lushman <andylushman@gmail.com>
2017-07-25 10:43:32 -0600

    Added questions

---
+++ questions.js
@@ -0,0 +1,24 @@
+var questions = [
+  {
+    full: "The Rocky Mountains are the longest mountain range in North America.",
+    cloze: "Rocky Mountains"
+  },
+  {
+    full: "The ancient Incan citadel Machu Picchu is located in Peru",
+    cloze: "Peru"
+  },
+  {
+    full: "The Easternmost point on the North American continent is the island of Newfoundland",
+    cloze: "Newfoundland"
+  },
+  {
+    full: "Beirut is the capital and largest city of Lebanon",
+    cloze: "Lebanon"
+  },
+  {
+    full: "Hindi is the national language of India",
+    cloze: "Hindi"
+  }
+] //End questions
+
+module.exports = questions;
